8ac90b8c9a327a2de5802c5a7b45c90d7f07946f
tleon <leone.thomas.92@gmail.com>
2019-03-14 17:59:03 +0100

    stylr footer header btn

--- footer_cat.php
+++ footer_cat.php
@@ -1,31 +1,31 @@
       <div class="container-fluid bg-dark text-light">
-        <div class="row justify-content-center back-footer">
+        <div class="row justify-content-center tom-nav-bg">
           <div class="col-lg-6 col-12">
             <div class="row">
               <div class="col-3 text-center">
-                <p class="font-weight-bold">REJOIGNER NOUS SUR:</p>
+                <p class="font-weight-bold tom-card-desc">REJOIGNER NOUS SUR:</p>
               </div>
               <div class="col-1 offset-1 text-center">
-                <i class="fab fa-facebook-f fa-2x" style="color: white;"></i>
+                <i class="fab fa-facebook-f fa-2x tom-card-desc"></i>
               </div>
               <div class="col-1 text-center">
-                <i class="fab fa-twitter fa-2x" style="color: white;"></i>
+                <i class="fab fa-twitter fa-2x tom-card-desc"></i>
               </div>
               <div class="col-1 text-center">
-                <i class="fab fa-instagram fa-2x" style="color: white;"></i>
+                <i class="fab fa-instagram fa-2x tom-card-desc"></i>
               </div>
             </div>
           </div>
           <div class="col-lg-6 col-12">
             <div class="row">
               <div class="col-3 text-center">
-                <p class="font-weight-bold">APPLI MOBILE:</p>
+                <p class="font-weight-bold tom-card-desc">APPLI MOBILE:</p>
               </div>
               <div class="col-1 offset-1 text-center">
-                <i class="fab fa-apple fa-2x" style="color: white;"></i>
+                <i class="fab fa-apple fa-2x tom-card-desc" ></i>
               </div>
               <div class="col-1 text-center">
-                <i class="fab fa-android fa-2x" style="color: white;"></i>
+                <i class="fab fa-android fa-2x tom-card-desc" ></i>
               </div>
             </div>
           </div>
@@ -38,77 +38,77 @@
             <div class="form-group">
               <div class="row">
                 <div class="col-12"><br>
-                  <p>1O euros Offert pour votre premier achat profiter en !</p>
+                  <p class="tom-card-desc">1O euros Offert pour votre premier achat profiter en !</p>
                 </div>
                 <div class="col-lg-6">
-                  <label for="exampleFormControlInput1">Email address</label>
+                  <label class="tom-card-desc" for="exampleFormControlInput1">Email address</label>
                   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
                   <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Your message</label>
+                    <label class="tom-card-desc" for="exampleFormControlTextarea1">Your message</label>
                     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea><br>
-                    <button class="btn btn-primary" type="submit">Submit</button>
+                    <button class="btn tom-hr" type="submit">Submit</button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
           <div class="col-lg-3"><br>
-            <h2>Wild bazar</h2>
-            <p>1 Rue de la paix<br>75 001 CEDEX<br>SAV 56 78<br>Recrutement<br>A propos de WildBeer</p>
+            <h2 class="tom-footer-title">Wild bazar</h2>
+            <p class="tom-card-desc">1 Rue de la paix<br>75 001 CEDEX<br>SAV 56 78<br>Recrutement<br>A propos de WildBeer</p>
           </div>
           <div class="col-lg-2"><br>
-            <h2>Nos partenaires</h2><br>
+            <h2 class="tom-footer-title">Nos partenaires</h2><br>
             <a href="https://urlz.fr/96tG">
-              <p>Wild Code School</p>
+              <p class="tom-card-desc">Wild Code School</p>
             </a>
             <a href="https://urlz.fr/96tG">
-              <p>Bière académie</p>
+              <p class="tom-card-desc">Bière académie</p>
             </a>
             <a href="https://urlz.fr/96tG">
-              <p>Maison de l'Alcool</p>
+              <p class="tom-card-desc">Maison de l'Alcool</p>
             </a>
           </div>
         </div>
-        <hr style="background-color: white;">
+        <hr class="tom-hr">
         <div class="row mx-auto">
           <div class="col-lg-3 col-12">
-            <i class="fab fa-fedex fa-2x" style="color: white;"></i>
-            <h4 class="font-weight-bold text-warning policefooter">Expedition rapide</h4>
-            <p>Emballage anti-casse envoyés sous 24h</p>
+            <i class="fab fa-fedex fa- tom-card-desc"></i>
+            <h4 class="font-weight-bold tom-footer-title policefooter">Expedition rapide</h4>
+            <p class="tom-card-desc">Emballage anti-casse envoyés sous 24h</p>
           </div>
           <div class="col-lg-3 col-12">
-            <i class="fab fa-tripadvisor fa-2x" style="color: white;"></i>
-            <h4 class="font-weight-bold text-warning policefooter">4,8/5</h4>
-            <p>Nos client nous adorent et nous leur rendons bien</p>
+            <i class="fab fa-tripadvisor fa-2x tom-card-desc"></i>
+            <h4 class="font-weight-bold tom-footer-title policefooter">4,8/5</h4>
+            <p class="tom-card-desc">Nos client nous adorent et nous leur rendons bien</p>
           </div>
           <div class="col-lg-3 col-12">
-            <i class="fab fa-untappd fa-2x" style="color: white;"></i>
-            <h4 class="font-weight-bold text-warning policefooter">Logo dans le thème</h4>
+            <i class="fab fa-untappd fa-2x tom-card-desc"></i>
+            <h4 class="font-weight-bold tom-footer-title policefooter">Logo dans le thème</h4>
             <p>...</p>
           </div>
           <div class="col-lg-3 col-12">
-            <i class="fab fa-dev fa-2x" style="color: white;"></i>
-            <h4 class="font-weight-bold text-warning policefooter">Tu devras</h4>
-            <p>N'est ce pas ?</p>
+            <i class="fab fa-dev fa-2x tom-card-desc" ></i>
+            <h4 class="font-weight-bold tom-footer-title policefooter">Tu devras</h4>
+            <p class="tom-card-desc">N'est ce pas ?</p>
           </div>
         </div>
         <div class="row justify-content-center iconefooter">
           <div class="col-lg-2 col-4">
-            <i class="fab fa-cc-mastercard fa-2x" style="color: white;"></i>
+            <i class="fab fa-cc-mastercard fa-2x tom-card-desc" ></i>
           </div>
           <div class="col-lg-2 col-4">
-            <i class="fab fa-paypal fa-2x" style="color: white;"></i>
+            <i class="fab fa-paypal fa-2x tom-card-desc"></i>
           </div>
           <div class="col-lg-2 col-4">
-            <i class="fab fa-cc-visa fa-2x" style="color: white;"></i>
+            <i class="fab fa-cc-visa fa-2x tom-card-desc"></i>
           </div>
         </div>
         <div class="row justify-content-center">
           <div class="col-12">
-            <p class="taillepolicefooter  ">(L'ABUS D'ALCOOL EST DANGEROUS POUR LA SANTÉ.)</p>
+            <p class="taillepolicefooter tom-card-desc ">(L'ABUS D'ALCOOL EST DANGEROUS POUR LA SANTÉ.)</p>
           </div>
         </div>
 
       </div>
 
-      <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+      <button  onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
