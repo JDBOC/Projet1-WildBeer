a44688f8d992fabad3f05f2cdbb98bff7742f617
Noel An <noel.an@hotmail.fr>
2019-03-15 11:39:07 +0100

    je naurai pas du travailler dessu

--- cat/card.php
+++ cat/card.php
@@ -81,11 +81,103 @@ foreach ($biereFrance as $key=>$biere) {
 </div>';
 }
 
-?>
+
 </div>
 </div>
 </section>
 </div>
+
+<!-- DEBUT partie popup  -->
+            <div id="AchelB" class="modal fade" role="dialog">
+              <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                  <div class="modal-header bg-dark text-light">
+                    <div class="container-fluid">
+                      <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-6">
+                          <!-- TITRE DU POPUP  -->
+                          <h4 class="modal-title">Köstritzer</h4>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-6">
+                          <!--  BOUTON CROIX FERMETURE -->
+                          <button type="button" class="close text-light" data-dismiss="modal">&times;</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="modal-body">
+                    <!-- CORPS DU POP UP  -->
+                    <div class="container-fluid">
+                      <div class="row">
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-4">
+                          <!--  IMAGE DANS LE POPUP -->
+                          <img class="card-img-top" src="../asset/img/noel/beers/achel.png" alt="Card image">
+                        </div>
+                        <div class="col-lg-8 col-md-8 col-sm-8 col-8">
+                          <div class="row">
+                            <table class="table table-borderless">
+                              <tr>
+                                <td class="align-middle font-weight-bold">Note</td>
+                                <td>
+                                  <table class="table table-bordered">
+                                    <!-- TABLEAU GOUT/SOIF  -->
+                                    <tr>
+                                      <td>Gout</td>
+                                      <td>Soif</td>
+                                      <td>Amertume</td>
+                                    </tr>
+                                    <tr>
+                                      <td>2/5</td>
+                                      <td>1.5/2</td>
+                                      <td>0/2</td>
+                                    </tr>
+                                  </table>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td class="font-weight-bold">Alcool/volume</td>
+                                <td>3.8%</td>
+                              </tr>
+                              <tr>
+                                <td class="font-weight-bold">Fermentation</td>
+                                <td>Basse</td>
+                              </tr>
+                            </table>
+                          </div>
+                          <div class="row">
+                            <!-- BOUTON RESEAU SOCIAUX  -->
+                            <button class="btn"><i class="fa fa-twitter"></i></button>
+                            <button class="btn"><i class="fa fa-facebook"></i></button>
+                            <button class="btn"><i class="fa fa-google-plus"></i></button>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                          <br><br>
+                          <table class="table">
+                            <!--TABLEAU PRIX   -->
+                            <tr>
+                              <td class="font-weight-bold">Unité</td>
+                              <td>7€</td>
+                            </tr>
+                            <tr>
+                              <td class="font-weight-bold">Caisse (x6)</td>
+                              <td>30 €</td>
+                            </tr>
+                            <tr>
+                              <td class="font-weight-bold">Fut</td>
+                              <td>100€</td>
+                            </tr>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
 
 <footer>
       <?php include('../footer.php');?>
