7c9d861eca9573e3de7657fa209216236013d58f
tleon <leone.thomas.92@gmail.com>
2019-03-16 17:59:26 +0100

    modified germany, reorganised folders added font

--- cat/germany.php
+++ cat/germany.php
@@ -1,40 +1,24 @@
 <?php include('../doctype.php');?>
+
 <body>
   <header>
     <?php include('../header_cat.php');?>
   </header>
-
   <div class="container-fluid tom-page-bg">
-    <section id="descripCat">
-      <br><br>
+    <?php include('germanyData/germanyJumbotron.php');?>
+    <section id="nosProduits">
+      <br />
+      <h3 class="tom-cat-title font-weight-bold tom-caviar text-center">Retrouve les bières d'Allemagne</h3><br />
       <div class="container">
-        <div class="row d-flex justify-content-center">
-          <h2 class="font-weight-bold tom-cat-title">Les Bierres allemandes</h2><br /><br />
+        <div class="row d-flex justify-content-around">
+          <?php include('germanyData/germany_card.php');?>
         </div>
-        <br><br>
-        <div class="row">
-          <p class="tom-cat-title tom-desc-size">
-            La bière allemande trouve son origine dans le haut Moyen Âge.
-            Depuis des siècles, la bière fait partie de la culture et de la
-            gastronomie allemande, et le pays est constellé de Bierpalast,
-            de Biergarten, de Bierstube1, de Bierkeller, de Bierhaus et Bierzelt
-            que l'on retrouve lors des nombreuses fêtes de la bière. </p>
-        </div>
-        <br><br>
       </div>
+      <br /><br />
     </section>
-  <section id="nosProduits">
-    <br /><h3 class="tom-cat-title font-weight-bold text-center">Retrouve les bières d'Allemagne</h3><br />
-    <div class="container">
-      <div class="row d-flex justify-content-around">
-        <?php include('germany_card.php');?>
-      </div>
-    </div>
-  </section>
   </div>
+  <footer>
+    <?php include('../footer_cat.php');?>
+  </footer>
 
-<footer>
-  <?php include('../footer_cat.php');?>
-</footer>
-
-<?php include('../scriptImport.php');?>
+  <?php include('../scriptImport.php');?>
